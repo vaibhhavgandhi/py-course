6f5ada8497432cf1046e8b7c599800ebd7b35f24
vaibhhavgandhi <vaibhavgandhi06@gmail.com>
2025-06-10 22:42:00 +0530

    Add files via upload
    DESCRIPTION: 
    
    Program 1:
    1. This program takes 2 numbers as separate inputs from the user.
    2. It then uses those inputs to display some basic mathematic operations; Addition, Subtraction, Multiplication and Division.
    
    Program 2: 
    1. This program takes the user's name and surname as separate inputs first.
    2. It then concatenates the 2 inputs and displays a personalized greeting message.

---
+++ day1.py
@@ -0,0 +1,15 @@
+"""#Program 1"""
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+a=int(a)
+b=int(b)
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+\
+"""#Program 2"""
+c = input("Enter your first name: ")
+d = input("Enter your second name: ")
+name = c+" "+d
+print("Hello, ",name,"! Welcome to the python program")
